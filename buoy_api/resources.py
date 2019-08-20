afb529ec0a856bc1d3eb1e2dada352f669d3f5e7
Jacob <jbmenashi@gmail.com>
2019-08-20 12:51:16 -0400

    working on fixtures

--- buoy_api/resources.py
+++ buoy_api/resources.py
@@ -1,13 +1,16 @@
 from tastypie.resources import ModelResource
 from buoy_api.models import Symptom, Result
+from tastypie.authorization import Authorization
 
 
 class SymptomResource(ModelResource):
     class Meta:
         queryset = Symptom.objects.all()
         resource_name = 'symptom'
+        authorization = Authorization()
 
 class ResultResource(ModelResource):
     class Meta:
         queryset = Result.objects.all()
         resource_name = 'result'
+        authorization = Authorization()
