2b8b66269a0d2b60520573da3ea186c2c7e39a8b
Jacob <jbmenashi@gmail.com>
2019-08-20 11:52:05 -0400

    make models and migrations

--- buoy_api/models.py
+++ buoy_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Symptom(models.Model):
+   title = models.CharField(max_length=100)
+
+class Result(models.Model):
+   title = models.CharField(max_length=100)
+   frequency = models.IntegerField()
+   symptom = models.ForeignKey(Symptom, on_delete=models.CASCADE)
